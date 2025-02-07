348b927443a04f54abd6e92f40e06af0fbcc21ec
Jannik <1337741710@qq.com>
2025-02-07 23:00:25 +0800

    feat: 提交代码

--- packages/versakit-dev/src/App.vue
+++ packages/versakit-dev/src/App.vue
@@ -29,6 +29,18 @@ import { VerTimeLineItem, VerTimeline } from '@versakit/ui'
           <p>初始化项目结构和依赖</p>
           <p>2025-01-01</p>
         </VerTimeLineItem>
+
+        <VerTimeLineItem color="orange">
+          <h4>创建项目</h4>
+          <p>初始化项目结构和依赖</p>
+          <p>2025-01-01</p>
+        </VerTimeLineItem>
+
+        <VerTimeLineItem color="green">
+          <h4>创建项目</h4>
+          <p>初始化项目结构和依赖</p>
+          <p>2025-01-01</p>
+        </VerTimeLineItem>
       </VerTimeline>
     </div>
   </div>
