26b297b3051c911b3c2032291150883063835209
abining <3094774079@qq.com>
2025-02-17 18:39:28 +0800

    docs: Calendar 日历组件文档编写

---
+++ docs/components/calendar/example/base.vue
@@ -0,0 +1,9 @@
+<template>
+  <VerCalendar v-model="value" />
+</template>
+
+<script lang="ts" setup>
+import { VerCalendar } from '@versakit/ui'
+import { ref } from 'vue'
+const value = ref(new Date())
+</script>
---
+++ docs/components/calendar/example/customize.vue
@@ -0,0 +1,21 @@
+<template>
+  <VerCalendar v-model="date">
+    <template #date-cell="{ data }">
+      <div class="custom-cell">
+        <div class="custom-cell-date" :class="{ 'is-selected': data.isSelected }">
+          {{ data.day.split('-').slice(2).join('') }}
+        </div>
+        <div class="custom-cell-text" v-if="data.type === 'current-month'">
+          {{ data.isSelected ? '已选择' : '可选' }}
+        </div>
+      </div>
+    </template>
+  </VerCalendar>
+</template>
+
+<script lang="ts" setup>
+import { VerCalendar } from '@versakit/ui'
+import { ref } from 'vue'
+
+const date = ref(new Date())
+</script>
