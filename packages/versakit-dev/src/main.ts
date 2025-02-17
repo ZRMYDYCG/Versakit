d778b4c08de8c1a9e2be563144ef587047b2ac77
abining <3094774079@qq.com>
2025-02-17 18:39:41 +0800

    fix: 修复Calendar 日历无法使用

--- packages/versakit-dev/src/main.ts
+++ packages/versakit-dev/src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Versakit from '@versakit/ui' // 引入组件库
+import '@versakit/ui/dist/style.css' // 引入样式
 const app = createApp(App)
 
+app.use(Versakit)
 app.mount('#app')
