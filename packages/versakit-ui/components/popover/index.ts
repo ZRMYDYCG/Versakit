64f89c6f28f04788ebf92e7d8ece34a1d8ed55a2
Simonmie <16605913446@163.com>
2025-03-11 23:24:19 +0800

    feat: 新建popover组件（待开发）

---
+++ packages/versakit-ui/components/popover/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from '../../utils/withinstall';
+        import popover from './src/index.vue';
+
+        const VerP = withInstall(popover);
+
+        export default VerP;
---
+++ packages/versakit-ui/components/popover/src/index.vue
@@ -0,0 +1,6 @@
+
+    <template>
+        <div>popover组件内容占位</div>
+    </template>
+
+    <script setup lang="ts"></script>
