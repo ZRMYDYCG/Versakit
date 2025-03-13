9c9410d796f0d1679bd3f2a40f463d7c98c579c9
zhuoyixin <1454047497@qq.com>
2025-03-13 21:05:54 +0800

    test(component): 添加 Flex 组件的单元测试

---
+++ packages/versakit-ui/components/flex/test/index.spec.ts
@@ -0,0 +1,107 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import type { AlignProps, JustifyProps, WrapProps } from '../type'
+import Flex from '../src/index.vue'
+
+describe('Flex Component', () => {
+  it('gap should be 8px when props.gap is "small"', () => {
+    const wrapper = mount(Flex, {
+      props: { gap: 'small' },
+    })
+    expect(wrapper.vm.gap).toBe('8px')
+  })
+
+  it('gap should be 16px when props.gap is "middle"', () => {
+    const wrapper = mount(Flex, {
+      props: { gap: 'middle' },
+    })
+    expect(wrapper.vm.gap).toBe('16px')
+  })
+
+  it('gap should be 24px when props.gap is "large"', () => {
+    const wrapper = mount(Flex, {
+      props: { gap: 'large' },
+    })
+    expect(wrapper.vm.gap).toBe('24px')
+  })
+
+  it('gap should be 5px when props.gap is a number 5', () => {
+    const wrapper = mount(Flex, {
+      props: { gap: 5 },
+    })
+    expect(wrapper.vm.gap).toBe('5px')
+  })
+
+  it('gap should be "5px 10px" when props.gap is an array [5, 10]', () => {
+    const wrapper = mount(Flex, {
+      props: { gap: [5, 10] },
+    })
+    expect(wrapper.vm.gap).toBe('5px 10px')
+  })
+
+  it('should apply "ver-flex--vertical" class when vertical is true', () => {
+    const wrapper = mount(Flex, {
+      props: { vertical: true },
+    })
+    expect(wrapper.classes()).toContain('ver-flex--vertical')
+  })
+
+  it('should not apply "ver-flex--vertical" class when vertical is false or undefined', () => {
+    const wrapperFalse = mount(Flex, {
+      props: { vertical: false },
+    })
+    const wrapperUndefined = mount(Flex)
+
+    expect(wrapperFalse.classes()).not.toContain('ver-flex--vertical')
+    expect(wrapperUndefined.classes()).not.toContain('ver-flex--vertical')
+  })
+
+  it.each<WrapProps>(['nowrap', 'wrap', 'wrap-reverse'])(
+    'should set --ver-flex-wrap to %s when wrap is %s',
+    (wrapValue) => {
+      const wrapper = mount(Flex, {
+        props: { wrap: wrapValue },
+      })
+      expect(wrapper.attributes('style')).toContain(
+        `--ver-flex-wrap: ${wrapValue};`,
+      )
+    },
+  )
+
+  it.each<[JustifyProps, JustifyProps]>([
+    ['normal', 'normal'],
+    ['flex-start', 'flex-start'],
+    ['center', 'center'],
+    ['flex-end', 'flex-end'],
+    ['space-between', 'space-between'],
+    ['space-around', 'space-around'],
+    ['space-evenly', 'space-evenly'],
+  ])(
+    'should set --ver-flex-justify to %s when justify is %s',
+    (justifyValue, expected) => {
+      const wrapper = mount(Flex, {
+        props: { justify: justifyValue },
+      })
+      expect(wrapper.attributes('style')).toContain(
+        `--ver-flex-justify: ${expected};`,
+      )
+    },
+  )
+
+  it.each<[AlignProps, JustifyProps]>([
+    ['normal', 'normal'],
+    ['flex-start', 'flex-start'],
+    ['center', 'center'],
+    ['flex-end', 'flex-end'],
+  ])(
+    'should set --ver-flex-align to %s when align is %s',
+    (alignValue, expected) => {
+      const wrapper = mount(Flex, {
+        props: { align: alignValue },
+      })
+      expect(wrapper.attributes('style')).toContain(
+        `--ver-flex-align: ${expected};`,
+      )
+    },
+  )
+})
