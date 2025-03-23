d13907d272c55764a72bfa97a749df14d064aeb1
Jannik <1337741710@qq.com>
2025-03-23 15:29:48 +0800

    docs: 文档更新

--- docs/.vitepress/config/getDesignSidebar.ts
+++ docs/.vitepress/config/getDesignSidebar.ts
@@ -3,8 +3,12 @@ import { DefaultTheme } from 'vitepress'
 export const getDesignSidebar = (): DefaultTheme.Sidebar => {
   return [
     {
-      text: '主题',
-      items: [{ text: '主题生成器', link: '/design/theme/' }],
+      text: '设计',
+      items: [
+        { text: '颜色', link: '/design/color/' },
+        { text: '主题生成器', link: '/design/theme/' },
+        { text: '图标', link: '/design/icon/' },
+      ],
     },
   ]
 }
--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -4,7 +4,6 @@ export const getNav = (): DefaultTheme.NavItem[] => {
   return [
     { text: '指南', link: '/guide/versakit/' },
     { text: '设计', link: '/design/theme/' },
-    { text: '图标库', link: '/icons/icon/' },
     { text: '组件', link: '/components/layout/' },
     { text: '指令集', link: '/directives/ripple/' },
   ]
