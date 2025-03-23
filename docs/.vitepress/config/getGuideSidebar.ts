eab7e5aca795546c2ab7915e4a4bb063f2e3b936
Jannik <1337741710@qq.com>
2025-03-23 12:26:30 +0800

    docs: 文档更新

--- docs/.vitepress/config/getGuideSidebar.ts
+++ docs/.vitepress/config/getGuideSidebar.ts
@@ -5,6 +5,7 @@ export const getGuideSidebar = (): DefaultTheme.Sidebar => {
     {
       text: '指南',
       items: [
+        { text: '基本介绍', link: '/guide/versakit/' },
         { text: '安装', link: '/guide/installation/' },
         { text: '快速开始', link: '/guide/quickstart/' },
         { text: '暗黑模式', link: '/guide/dark/' },
--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from 'vitepress'
 
 export const getNav = (): DefaultTheme.NavItem[] => {
   return [
-    { text: '指南', link: '/guide/installation/' },
+    { text: '指南', link: '/guide/versakit/' },
     { text: '主题', link: '/design/theme/' },
     { text: '组件', link: '/components/layout/' },
     { text: '指令集', link: '/directives/ripple/' },
