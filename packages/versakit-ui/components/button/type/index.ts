49a0a5ffe962ce355ef226c7fc1b72901a1b008a
Jannik <1337741710@qq.com>
2025-04-18 21:19:52 +0800

    feat: btn unstyled�

--- packages/versakit-ui/components/button/type/index.ts
+++ packages/versakit-ui/components/button/type/index.ts
@@ -1,5 +1,11 @@
 type BtnType = 'primary' | 'secondary' | 'success' | 'error' | 'warn'
 
+export interface PtProps {
+  root?: string | Record<string, any>
+  icon?: string | Record<string, any>
+  label?: string | Record<string, any>
+}
+
 export interface ButtonProps {
   size?: string
   disabled?: boolean
@@ -7,4 +13,6 @@ export interface ButtonProps {
   type?: BtnType | ''
   icon?: string
   variant?: string
+  unstyled?: boolean
+  pt?: PtProps
 }
