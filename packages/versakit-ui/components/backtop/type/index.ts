2de33c7cf72dbcfaa5eff24296066c94d7b25ed4
ZRMYDYCG <547471919@qq.com>
2025-05-08 13:29:34 +0800

    feat: (vk-backtop) 重构 backtop 组件，同时支持可以选择不同的触发Dom

--- packages/versakit-ui/components/backtop/type/index.ts
+++ packages/versakit-ui/components/backtop/type/index.ts
@@ -1,6 +1,6 @@
 export interface PtProps {
   /* backTop容器的自定义类名 */
-  root?: string
+  root?: string | Record<string, any>
 }
 
 export interface BackTopProps {
@@ -10,6 +10,8 @@ export interface BackTopProps {
   icon?: string
   iconColor?: string
   visibilityHeight?: string
+  /*目标元素的选择器或元素*/
+  target?: string | HTMLElement
   /** 是否禁用默认样式 */
   unstyled?: boolean
   pt?: PtProps
