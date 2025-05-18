2597994cf8b28e3c00c590eca8982a3931662c0a
ZRMYDYCG <547471919@qq.com>
2025-05-18 23:23:45 +0800

    docs: 文档更新

--- packages/versakit-ui/components/image/type/index.ts
+++ packages/versakit-ui/components/image/type/index.ts
@@ -1,6 +1,12 @@
+export interface ImagePtProps {
+  root?: string | Record<string, any>
+}
+
 export interface ImageProps {
   src: string
   fit?: 'fill' | 'contain' | 'cover' | 'none' | 'scale-down'
   lazy?: boolean
   icon?: string
+  unstyled?: boolean
+  pt?: ImagePtProps
 }
